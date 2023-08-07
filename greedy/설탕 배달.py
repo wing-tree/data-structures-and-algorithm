e1aa639b2d821b78bdce3edc1661c3efb027724e
KIM SEONGJU <80195353+wing-tree@users.noreply.github.com>
2023-08-08 01:17:24 +0900

    Create 설탕 배달.py

---
+++ greedy/설탕 배달.py
@@ -0,0 +1,16 @@
+N = int(input())
+n = 0
+m = 0
+
+while N > 0:
+    if N % 5 == 0:
+        n = int(N / 5)
+        break
+    else:
+        N -= 3
+        m += 1
+
+if N < 0:
+    print(-1)
+else:
+    print(n + m)
