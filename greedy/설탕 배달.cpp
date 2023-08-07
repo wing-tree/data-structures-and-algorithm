ea8cdcf984f6aa13fac38372520ea6690ca1789c
KIM SEONGJU <80195353+wing-tree@users.noreply.github.com>
2023-08-08 01:11:58 +0900

    Create 설탕 배달.cpp

---
+++ greedy/설탕 배달.cpp
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/2839
+
+#include <cstdio>
+
+int main() {
+	int N(0), n(0), m(0);
+    
+    scanf("%d", &N);
+
+    while (N > 0) {
+        if (N % 5 == 0) {
+            n = N / 5;
+            break;
+        } else {
+            N -= 3;
+            ++m;
+        }
+    }
+
+    if (N < 0) {
+        printf("-1");
+    } else {
+        printf("%d", n + m);
+    }
+
+	return 0;
+}
