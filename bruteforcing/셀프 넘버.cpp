564102f0cd34119073d81cedb8db95f1cc7201c3
KIM SEONGJU <80195353+wing-tree@users.noreply.github.com>
2023-08-16 02:20:09 +0900

    Create 셀프 넘버.cpp

---
+++ bruteforcing/셀프 넘버.cpp
@@ -0,0 +1,33 @@
+
+
+#include <cstdio>
+#include <vector>
+
+std::vector<bool> N(10001, true);
+
+int d(int n) {
+    int digit;
+	  int m(n);
+
+    while (n > 0) {
+        digit = n % 10;
+        m += digit;
+        n /= 10;
+    }
+
+    return m;
+}
+
+int main() {
+    for (int i(1); i < 10001; ++i) {
+        N[d(i)] = false;
+	  }
+
+    for (int i(1); i < 10001; ++i) {
+        if (N[i]) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
